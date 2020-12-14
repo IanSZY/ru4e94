a5acd9984058ad8d176d320c5c64ce337ffca89e
iansu7 <ian880124@gmail.com>
2020-12-14 16:08:04 +0800

    one commit

---
+++ history.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Wed Sep 30 15:45:34 2020)---
+debugfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+plt.xlabel('some numbers')
+plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+import matplotlib.pyplot as plt
+x = [1,2,3,4]
+y = [4,8,12,16]
+plt.plot(x,y)
+plt.xlabel('some numbers')
+plt,show()
+plt.show()
+import matplotlib.pyplot as plt
+x = [1,2,3,4]
+y = [4,8,12,16]
+plt.plot(x,y)
+plt.xlabel('some numbers')
+plt.show()
+runfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+from requests.exceptions import ReadTimeout
+import time
+import pandas as pd
+runfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+pip install ptt.py
+from ptt import 
+runfile('C:/Users/user/.spyder-py3/ppt.py', wdir='C:/Users/user/.spyder-py3')
+pip install ptt.py
+runfile('C:/Users/user/.spyder-py3/ppt.py', wdir='C:/Users/user/.spyder-py3')
+runfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+runfile('C:/Users/user/.spyder-py3/ppt.py', wdir='C:/Users/user/.spyder-py3')
+runfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+# -*- coding: utf-8 -*-
+"""
+runfile('C:/Users/user/.spyder-py3/ptt.py', wdir='C:/Users/user/.spyder-py3')
+Created on Mon Apr 22 21:55:23 2019
+@author: Howie
+"""
+from requests.exceptions import ReadTimeout
+import time
+import pandas as pd
+import os
+import urllib
+import datetime
+import collections
+import json
+import argparse
+import requests
+from bs4 import BeautifulSoup
+# exception
+class Error(Exception):
+"""Base class for all exceptions raised by this module"""
+pass
+class InValidBeautifulSoupTag(Error):
+"""Can not create ArticleSummary because of invalid bs tag"""
+pass
+class NoGivenURLForPage(Error):
+"""Given None or empty url when build page"""
+pass
+class PageNotFound(Error):
+"""Can not fetch page by given url"""
+pass
+runfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
